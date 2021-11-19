fba42fadc80b79a22de5468f55ac2e32515a62ce
Diego Soares <as.diego@live.com>
2021-11-19 12:24:37 -0300

    solve q1

---
+++ hello-python.py
@@ -0,0 +1,14 @@
+# Question 0
+# create a variable called color with an appropriate value on the line below
+# (Remember, strings in Python must be enclosed in 'single' or "double" quotes)
+color = "blue"
+
+# Question 1
+pi = 3.14159 # approximate
+diameter = 3
+
+# Create a variable called 'radius' equal to half the diameter
+radius = diameter/2
+
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+area = pi * radius ** 2
