3e6a04731df07302f2b3fba8f911a373a00029fe
Diego Soares <as.diego@live.com>
2021-11-19 12:25:49 -0300

    solve q3b

--- hello-python.py
+++ hello-python.py
@@ -36,3 +36,9 @@ b = c
 #Add parentheses to the following expression so that it evaluates to 1.
 # 5 - 3 // 2
 (5 - 3) // 2
+
+# Question 3b
+# Add parentheses to the following expression so that it evaluates to 0.
+# 8 - 3 * 2 - 1 + 1
+8 - 3 * 2 - (1 + 1)
+
