1d4e0fbb14340f708b135abb59167338a04bf605
Diego Soares <as.diego@live.com>
2021-11-19 12:27:02 -0300

    solve q4

--- hello-python.py
+++ hello-python.py
@@ -42,3 +42,16 @@ b = c
 # 8 - 3 * 2 - 1 + 1
 8 - 3 * 2 - (1 + 1)
 
+# Question 4
+# Alice, Bob and Carol have agreed to pool their Halloween candy and split it evenly among themselves. For the sake of their friendship, any candies left over will be smashed. For example, if they collectively bring home 91 candies, they'll take 30 each and smash 1.
+
+# Write an arithmetic expression below to calculate how many candies they must smash for a given haul.
+
+# Variables representing the number of candies collected by alice, bob, and carol
+alice_candies = 121
+bob_candies = 77
+carol_candies = 109
+
+# Your code goes here! Replace the right-hand side of this assignment with an expression
+# involving alice_candies, bob_candies, and carol_candies
+to_smash = (alice_candies + bob_candies + carol_candies) % 3
