74ff4d969d5905f7bd56ef310e73f18b21b8453f
Diego Soares <as.diego@live.com>
2021-11-19 12:25:35 -0300

    solve q3a

--- hello-python.py
+++ hello-python.py
@@ -32,3 +32,7 @@ b = c
 ######################################################################
 
 
+# Question 3a
+#Add parentheses to the following expression so that it evaluates to 1.
+# 5 - 3 // 2
+(5 - 3) // 2
