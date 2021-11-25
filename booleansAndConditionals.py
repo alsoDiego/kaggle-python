41d320f6b1b4d2f833495cdc397fba0c7c493c56
Diego Soares <as.diego@live.com>
2021-11-25 17:58:02 -0300

    solve q5q

--- booleansAndConditionals.py
+++ booleansAndConditionals.py
@@ -74,3 +74,10 @@ def wants_all_toppings(ketchup, mustard, onion):
     """Return whether the customer wants "the works" (all 3 toppings)
     """
     return ketchup and mustard and onion
+
+# q5b
+def wants_plain_hotdog(ketchup, mustard, onion):
+    """Return whether the customer wants a plain hot dog with no toppings.
+    """
+    return not ketchup and not mustard and not onion
+
