7532eb038ce80a44a453ce5c224441f3c58b430b
Diego Soares <as.diego@live.com>
2021-11-25 17:49:05 -0300

    solve q2a

--- booleansAndConditionals.py
+++ booleansAndConditionals.py
@@ -7,3 +7,16 @@ def sign(num):
     else:
         return 1
 ​
+# q2a
+def to_smash(total_candies):
+    """Return the number of leftover candies that must be smashed after distributing
+    the given number of candies evenly between 3 friends.
+    
+    >>> to_smash(91)
+    1
+    """
+    print("Splitting", total_candies, "candies")
+    return total_candies % 3
+
+to_smash(91)
+
