0ea5042ee236eb65c7cf77c465cba144587e353e
Diego Soares <as.diego@live.com>
2021-11-26 13:16:33 -0300

    solve q2

--- loopsAndListComprehensions.py
+++ loopsAndListComprehensions.py
@@ -26,3 +26,24 @@ My solution passed all tests. The given solutions are:
         return any([num % 7 == 0 for num in nums])
 """
 
+# q2
+def elementwise_greater_than(L, thresh):
+    """Return a list with the same length as L, where the value at index i is 
+    True if L[i] is greater than thresh, and False otherwise.
+    
+    >>> elementwise_greater_than([1, 2, 3, 4], 2)
+    [False, False, True, True]
+    """
+    is_greater = []
+    for i in L:
+        is_greater.append(i > thresh)
+    
+    return is_greater
+
+"""
+My solution passed all tests. The another given solution is:
+
+    def elementwise_greater_than(L, thresh):
+        return [ele > thresh for ele in L]
+"""
+
