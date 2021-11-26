b39ae444e8419eb1f0182ca62b00bb0de55b8b3f
Diego Soares <as.diego@live.com>
2021-11-26 16:34:37 -0300

    add q0

---
+++ stringsAndDictionaries.py
@@ -0,0 +1,7 @@
+# q0
+"""
+Let's start with a string lightning round to warm up. What are the lengths of the strings below?
+
+For each of the five strings below, predict what len() would return when passed that string. Use the variable length to record your answer, then run the cell to check whether you were right.
+"""
+
