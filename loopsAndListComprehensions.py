030ea09f7376bcf7265e5143bdf92906aed3f561
Diego Soares <as.diego@live.com>
2021-11-26 14:13:20 -0300

    solve q4

--- loopsAndListComprehensions.py
+++ loopsAndListComprehensions.py
@@ -58,3 +58,33 @@ def menu_is_boring(meals):
             return True
     return False
 
+# q4
+def estimate_average_slot_payout(n_runs):
+    """Run the slot machine n_runs times and return the average net profit per run.
+    Example calls (note that return value is nondeterministic!):
+    >>> estimate_average_slot_payout(1)
+    -1
+    >>> estimate_average_slot_payout(1)
+    0.5
+    """
+    pass
+
+    ### Given solution:
+    """
+    The exact expected value of one pull of the slot machine is 0.025 - i.e. a little more than 2 cents. See? Not every game in the Python Challenge Casino is rigged against the player!
+
+    In order to get this answer, you'll need to implement the estimate_average_slot_payout(n_runs) function to simulate pulling the slot machine n_runs times. It should return the payout averaged over those n_runs.
+
+    Then, once the function is defined, in order to estimate the average slot payout, we need only call the function.
+
+    Because of the high variance of the outcome (there are some very rare high payout results that significantly affect the average) you might need to run your function with a very high value of n_runs to get a stable answer close to the true expectation. For instance, you might use a value for n_runs of 1000000.
+
+    Here's an example for how the function could look:
+    """
+    def estimate_average_slot_payout(n_runs):
+        # Play slot machine n_runs times, calculate payout of each
+        payouts = [play_slot_machine()-1 for i in range(n_runs)]
+        # Calculate the average value
+        avg_payout = sum(payouts) / n_runs
+        return avg_payout
+
